7e0f2616b548134e8b621ca9126418dac767197e
soh003 <149380282+soh003@users.noreply.github.com>
2024-08-27 13:36:04 +0200

    opgave 1 instansfelter properties konstruktør

---
+++ ClassRoomNet60/Studerende.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Studerende
+    {
+
+        //Instansfelter
+        private string _navn;
+        private int _fødselsmåned;
+        private int _fødselsdag;
+
+        
+
+
+
+        //properties
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+        public int Fødselsmåned
+        {
+            get { return _fødselsmåned; }
+            set { _fødselsmåned = value; }
+        }
+
+        public int Fødselsdag
+        {
+            get { return _fødselsdag; }
+            set { _fødselsdag= value; }
+        }
+
+        //konstruktør
+        public Studerende(string navn, int fødselsmåned, int fødselsdag)
+        {
+            Navn = navn;
+            Fødselsmåned = fødselsmåned;
+            Fødselsdag = fødselsdag;
+        }
+    }
+}
